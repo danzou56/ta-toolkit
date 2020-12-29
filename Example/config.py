22a630306316fab7ddd12392ed30ddd85a7ee689
Alejandro Flores V <alejandroflores.af@gmail.com>
2020-12-29 16:11:10 -0500

    Adding example project

---
+++ Example/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.2
+
+config = {
+	'tas' : [
+		'Alejandro',
+		'Vlad',
+		'Dan',
+	],
+	'extensions' : [
+		'.java',
+		'.doc',
+		'.docx',
+	],
+	'files' : [
+		('file', 'Report.doc'),
+		('folder', 'src'),
+		('folder_with', 'File1.java'),
+	]
+}
