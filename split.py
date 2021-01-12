cd3625aad512bdf8393b00f7a9156870be0f069c
Dan Z <danny81299@gmail.com>
2021-01-11 21:01:32 -0500

    Use argparse

--- split.py
+++ split.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.2
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -9,77 +9,95 @@ import hashlib
 import shutil
 from distutils.dir_util import copy_tree
 
+def distribute(assignment_dir):
+	from ta import normalize_ta_list, ta_list
+	sys.path.insert(0, assignment_dir)
+	from config import config
+
+	# Filtering the list
+	if 'tas' in config:
+		ta_list = [ta for ta in ta_list if not ta.name not in config['tas']]
+
+	ta_list = normalize_ta_list(ta_list)
+
+	# The random generator should be consistent when running the code
+	#  for the same assigment. In this way, modifications can be done without
+	#  affecting the students assigned to each TA. To achieve this, the seed
+	#  for the random library is set using the hash of the assignment name.
+	random.seed(int(hashlib.sha1(assignment_dir.encode("utf-8")).hexdigest(), 16))
+	random.shuffle(ta_list)
+
+	# +====================+
+	# | Assignment handler |
+	# +====================+
+
+	def move_assignment(student_dir, ta, instructions):
+		student = student_dir.split('-',1)[0]
+		student_raw_path = os.path.join(assignment_dir, 'raw', student_dir)
+		student_new_path = os.path.join(assignment_dir, 'dist', ta.name, student)
+		os.makedirs(student_new_path, exist_ok=True)
+		for (inst, inst_path) in instructions:
+			for root, dirs, files in os.walk(student_raw_path):
+				if (inst == 'file' and inst_path in files):
+					# COPY ONLY THE FILE
+					shutil.copyfile(
+						os.path.join(root, inst_path),
+						os.path.join(student_new_path, inst_path)
+					)
+				elif ((inst == 'folder' and inst_path == os.path.basename(root)) or
+					(inst == 'folder_with' and inst_path in files)):
+					# COPY THE ENTIRE FOLDER
+					if not set(config['extensions']).isdisjoint({os.path.splitext(f)[1] for f in files}):
+						copy_tree(root, student_new_path)
 
 
+	student_dir = sorted(os.listdir(assignment_dir + '/raw'))
+	student_dir.remove('.DS_Store')
+	total_students = len(student_dir)
+	print('Students: {}'.format(total_students))
 
-if len(sys.argv) != 2:
-	print('Remember to indicate the assignment folder.\nExample: python3 split.py P6')
-	sys.exit()
+	print('TA assignments:')
+	ta_assignment = list(math.ceil(total_students * ta.percentage) for ta in ta_list)
+	ta_assignment[-1] -= sum(ta_assignment) - total_students
+	for (ta, num) in zip(ta_list, ta_assignment):
+		print('  · {}: {} students'.format(ta.name, num))
 
-assignment_dir = sys.argv[1]
+	assigment_split = [0] + list(np.cumsum(ta_assignment))[:-1]
 
-from ta import *
-sys.path.insert(0, assignment_dir)
-from config import config
+	shutil.rmtree(os.path.join(assignment_dir, 'dist'), ignore_errors=True)
+	os.makedirs(os.path.join(assignment_dir, 'dist'), exist_ok=True)
 
-# Filtering the list
-if 'tas' in config:
-	ta_list = [ta for ta in ta_list if not ta.name not in config['tas']]
-
-ta_list = normalize_ta_list(ta_list)
-
-# The random generator should be consistent when running the code
-#  for the same assigment. In this way, modifications can be done without
-#  affecting the students assigned to each TA. To achieve this, the seed
-#  for the random library is set using the hash of the assignment name.
-random.seed(int(hashlib.sha1(assignment_dir.encode("utf-8")).hexdigest(), 16))
-random.shuffle(ta_list)
+	for (ta, index, num) in zip(ta_list, assigment_split, ta_assignment):
+		os.makedirs(os.path.join(assignment_dir, 'dist', ta.name), exist_ok=True)
+		ta.setStudents(student_dir[index : (index + num)])
+		for student in ta.students:
+			move_assignment(student, ta, config['files'])
 
 
-# +====================+
-# | Assignment handler |
-# +====================+
+if __name__ == '__main__':
+	from argparse import ArgumentParser, RawTextHelpFormatter
 
+	parser = ArgumentParser(
+		description='Distribute grading of student project submissions among TAs. \n\n'
+					'After running the script, a new folder called dist '
+					'will be created as a subdirectory of assignment_dir '
+					'containing TA names. Each TA folder will have their '
+					'assigned students. \n\n'
+					'example usage:\n'
+					'python3 split.py P6\n'
+					'./split.py P6',
+		formatter_class=RawTextHelpFormatter
+	)
+	parser.add_argument('assignment_dir', help='Directory where student submissions are located.\n'
+											   'Directory structure should be as follows: \n'
+											   'assignment_dir/\n'
+											   '├── raw/\n'
+											   '│   ├── student1/\n'
+											   '│   ├── student2/\n'
+											   '│   └── ...\n'
+											   '└── config.py'
+						)
 
+	args = parser.parse_args()
 
-def move_assignment(student_dir, ta, instructions):
-	student = student_dir.split('-',1)[0]
-	student_raw_path = os.path.join(assignment_dir, 'raw', student_dir)
-	student_new_path = os.path.join(assignment_dir, 'dist', ta.name, student)
-	os.makedirs(student_new_path, exist_ok=True)
-	for (inst, inst_path) in instructions:
-		for root, dirs, files in os.walk(student_raw_path):
-			if (inst == 'file' and inst_path in files):
-				# COPY ONLY THE FILE
-				shutil.copyfile(
-					os.path.join(root, inst_path),
-					os.path.join(student_new_path, inst_path)
-				)
-			elif ((inst == 'folder' and inst_path == os.path.basename(root)) or
-				(inst == 'folder_with' and inst_path in files)):
-				# COPY THE ENTIRE FOLDER
-				if not set(config['extensions']).isdisjoint({os.path.splitext(f)[1] for f in files}):
-					copy_tree(root, student_new_path)
-
-
-student_dir = sorted(os.listdir(assignment_dir + '/raw'))
-student_dir.remove('.DS_Store')
-total_students = len(student_dir)
-print('Students: {}'.format(total_students))
-
-print('TA assignments:')
-ta_assignment = list(math.ceil(total_students * ta.percentage) for ta in ta_list)
-ta_assignment[-1] -= sum(ta_assignment) - total_students
-for (ta, num) in zip(ta_list, ta_assignment):
-	print('  · {}: {} students'.format(ta.name, num))
-
-assigment_split = [0] + list(np.cumsum(ta_assignment))[:-1]
-
-shutil.rmtree(os.path.join(assignment_dir, 'dist'), ignore_errors=True)
-os.makedirs(os.path.join(assignment_dir, 'dist'), exist_ok=True)
-
-for (ta, index, num) in zip(ta_list, assigment_split, ta_assignment):
-	os.makedirs(os.path.join(assignment_dir, 'dist', ta.name), exist_ok=True)
-	ta.setStudents(student_dir[index : (index + num)])
-	for student in ta.students:
-		move_assignment(student, ta, config['files'])
+	distribute(args.assignment_dir)
