78bd45d0bdba265a40126591186b0b82d4ee9935
Dan Zou <danny81299@gmail.com>
2021-03-21 20:55:44 -0400

    Distribute students correctly

--- split.py
+++ split.py
@@ -53,17 +53,20 @@ def distribute(assignment_dir):
 					if not set(config['extensions']).isdisjoint({os.path.splitext(f)[1] for f in files}):
 						copy_tree(root, student_new_path)
 
-	student_dir = sorted(f.name for f in os.scandir(raw_path) if f.is_dir())
+	student_dir = sorted(f.name for f in os.scandir(raw_path) if f.is_dir() and not f.name[:8] == 'psadeghi')
 	total_students = len(student_dir)
 	print('Students: {}'.format(total_students))
 
 	print('TA assignments:')
-	rem = total_students % len(ta_list)
 	ta_assignment = list(
-		math.floor(total_students * ta.percentage) + (1 if i < rem else 0) for i, ta in enumerate(ta_list)
+		math.floor(total_students * ta.percentage) for i, ta in enumerate(ta_list)
 	)
+	rem = total_students - sum(ta_assignment)	
+	if rem > 0:
+		ta_assignment = list(p + (1 if i < rem else 0) for i, p in enumerate(ta_assignment))
 	for (ta, num) in zip(ta_list, ta_assignment):
 		print('  · {}: {} students'.format(ta.name, num))
+	assert(sum(ta_assignment) == total_students, "Number of students to grade didn't match ta assignment!")
 
 	shutil.rmtree(new_path, ignore_errors=True)
 	os.makedirs(new_path, exist_ok=True)
