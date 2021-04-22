b17ef7084d5d9cb886305eb89262c2101ad341d3
Dan Zou <danzou12@umd.edu>
2021-04-22 15:16:38 -0400

    Pipify????

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='ta-toolkit',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Dan Zou',
+    author_email='danzou12@umd.edu',
+    description='Tools for TA\'ing with UMD services',
+    install_requires=[
+        'mossum @ git+https://github.com/danzou56/mossum.git#mossum'
+    ]
+)
